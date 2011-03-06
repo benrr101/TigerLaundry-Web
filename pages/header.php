c8aa562a13c32e49971d8a388bc7414ce8e6a6fa
Benjamin Russell <brr1922@rit.edu>
2011-03-06 03:40:25 +0000

    The 'new' way of drawing the header is really too flexible. Plus, it'll be a pain to make it look good on all OS's, so introducing, the new and improved, ye-olden header!

--- pages/header.php
+++ pages/header.php
@@ -10,6 +10,24 @@
 ?>
 
 				<a href="index.php?id=index"><img id="headerLogo" src="./images/headerLogo.png" alt="Tiger Laundry" /></a>
-				<div id="headerNav">
-					Some shit goes here.
+				<div id="headerHome" class="headerNav"><a href="index.php?page=home"><img src="./images/headerHome.png" alt="Home" /></a></div>				
+				<div id="headerDot1" class="headerNav"><img src="./images/headerDot.png" alt="&middot;" /></div>
+				<div id="headerBldg" class="headerNav">
+					<div id="headerBldgTrigger">
+						<a href="index.php?page=locationindex"><img src="./images/headerBldg.png" alt="Buildings" /></a>
+						<div id="headerBldgMenu">
+							List of buildings goes here
+						</div>
+					</div>
+				</div>
+				<div id="headerDot2" class="headerNav"><img src="./images/headerDot.png" alt="&middot;" /></div>
+				<div id="headerAbout" class="headerNav">
+					<div id="headerAboutTrigger">
+						<a href="index.php?page=about"><img src='./images/headerAbout.png' alt='About' /></a>
+						<div id="headerAboutMenu">
+							<p><a href="index.php?page=aboutgoals">Our Goals</a></p>
+							<p><a href="index.php?page=aboutteam">The Team</a></p>
+							<p><a href="index.php?page=aboutsource">The Source</a></p>
+						</div>
+					</div>
 				</div>
