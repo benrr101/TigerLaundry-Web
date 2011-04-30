6dfc352a16430f051ca8e837a61ddf70b3180f0d
Benjamin Russell <brr1922@rit.edu>
2011-04-30 03:48:22 +0000

    A script to update times on the machines

---
+++ tick.php
@@ -0,0 +1,26 @@
+<?php
+/* Ticking Script for TigerLaundry (2011 Recode Version)
+ * 
+ * This script is called by cron every minute of every day to subtract a minute
+ * from every machine that is running. It also changes the status of machines
+ * when their time goes to zero.
+ * 
+ * @author	Ben Russell (benrr101@csh.rit.edu)
+ */
+
+// REQUIRED CLASSES ========================================================
+require_once './includes/MySQLConnection.php';
+
+// Conn the database
+$dbConn = MySQLConnection::getInstance();
+
+// Run the mighty query
+$query = "UPDATE machines SET timeRemaining=timeRemaining-1, status= CASE timeRemaining-1 WHEN 0 THEN 'GREEN' ELSE 'RED' END WHERE timeRemaining > 0";
+$dbConn->query($query);
+if($dbConn->getLastError()) {
+	die('error:Database');
+}
+
+echo "success";
+
+?>
